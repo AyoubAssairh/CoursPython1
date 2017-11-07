7a64522c7978d4b265ba879e1c97db49b8ed17e2
AyoubAssairh <32266767+AyoubAssairh@users.noreply.github.com>
2017-11-07 15:36:37 +0100

    ras + ajour du fichier "for"

---
+++ For.py
@@ -0,0 +1,13 @@
+#Afficher des valeurs de 1 à 10
+
+#POUR i de 1 A 10 PAS DE 1
+#      afficher()
+#FINPOUR
+
+for 1 in range(1,11):
+    print(1)
+
+ma_liste =  ['oeufs','beurre','chocolat']
+print("Ma liste de courses : ")
+for item in ma_liste:
+    print(item)
--- variables.py
+++ variables.py
@@ -1,3 +1,11 @@
-score = 0
-nom_joueur = 'Dimitri'
-score = score +1
+#On demande au joueur son nom et son score précédent
+nom_joueur = input('Saisissez votre nom :')
+score = input('Saissez votre score précédent :')
+
+if score.isdigit():
+    score = int(score) + 1
+    print(nom_joueur,'a obtenu', score, 'points.')
+else:
+    print("Erreur !!")
+
+print(nom_joueur,'a obtenu', score,'points.')
