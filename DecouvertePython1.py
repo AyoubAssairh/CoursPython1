b757e8900fcbb6ebee7fe9a07089ea1af798f1a8
AyoubAssairh <32266767+AyoubAssairh@users.noreply.github.com>
2017-11-07 14:10:36 +0100

    test commit depui Visual Studio Code

---
+++ DecouvertePython1.py
@@ -0,0 +1,10 @@
+# Une variable se nomme en minsucules avec des _ entre les mots
+nom_joueur
+#Idem pour les fonctions et léthides
+def ma_fonction():
+    pass
+
+class MyClass: Les classe CamelCase.
+    pass
+#Les constantes sont des variabes nommérd en majuscuoe
+MA_CONSTANTE = 3.14
---
+++ variables.py
@@ -0,0 +1,3 @@
+score = 0
+nom_joueur = 'Dimitri'
+score = score +1
